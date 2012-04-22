29e8691424a1a5ca47b347a9c4ca8313b922b715
Miles Sandlar <miles.sandlar@gmail.com>
2012-04-22 19:21:37 -0400

    Some simplification of the code

--- course-add.rb
+++ course-add.rb
@@ -4,9 +4,10 @@ require 'nokogiri'
 require 'stringio'
 require 'highline/import'
 
-#Start of the Semester
+#Change based on Semester
 $term = '09'
 $year = '2012'
+
 $agent = Mechanize.new
 $agent.redirect_ok = true 
 $agent.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.11 Safari/535.19"
@@ -29,7 +30,6 @@ class String
 	end
 end
 
-
 #Logins, Gets the Courses, Returns Courses Obj with Name/URL/Tools for each
 def login(username, password)
 
@@ -42,18 +42,16 @@ def login(username, password)
 	})
 	if (login.submit().body.match(/Invalid username or password/)) then
 		return false
-	else 
-		return true 
+	else
+		return true
 	end
 end
 
+#Gets Course Information
 def getCourse(crn)	
-	courseDetails = Nokogiri::HTML(
-		$agent.get(
-			"https://banweb.banner.vt.edu/ssb/prod/HZSKVTSC.P_ProcComments?CRN=#{crn}&TERM=#{$term}&YEAR=#{$year}"
-		).body
-	)
-
+	courseDetails = Nokogiri::HTML( $agent.get(
+		"https://banweb.banner.vt.edu/ssb/prod/HZSKVTSC.P_ProcComments?CRN=#{crn}&TERM=#{$term}&YEAR=#{$year}"
+	).body)
 
 	#Flatten table to make it easier to work with
 	course = {}
@@ -92,12 +90,14 @@ def getCourse(crn)
 	return course
 end
 
+#Registers you for the given CRN, returns true if successful, false if not
 def registerCrn(crn)
+	#Follow Path
 	$agent.get("https://banweb.banner.vt.edu/ssb/prod/twbkwbis.P_GenMenu?name=bmenu.P_MainMnu")
-	registrationPage = "https://banweb.banner.vt.edu/ssb/prod/hzskstat.P_DispRegStatPage"
-	reg = $agent.get(registrationPage)	
-
+	reg = $agent.get("https://banweb.banner.vt.edu/ssb/prod/hzskstat.P_DispRegStatPage")
 	dropAdd = reg.link_with(:href => "/ssb/prod/bwskfreg.P_AddDropCrse?term_in=#{year}#{term}").click
+
+	#Fill in CRN Box and Submit
 	crnEntry = dropAdd.form_with(:action => '/ssb/prod/bwckcoms.P_Regs')
 	crnEntry.fields_with(:id => 'crn_id1').first.value = crn
 	crnEntry['CRN_IN'] = crn
@@ -110,8 +110,8 @@ def registerCrn(crn)
 	end
 end
 
-#Main loop
-def courseAdd(courses)
+#Main loop that checks the availaibility of each courses and fires to registerCrn on availaibility
+def checkCourses(courses)
 	
 	loop do
 		system("clear")
@@ -143,6 +143,42 @@ def courseAdd(courses)
 	end
 end
 
+#Add courses to be checked
+def addCourses 
+	crns = []
+
+	loop do 
+		system("clear")
+		puts "Your CRNs:".color(:red)
+		crns.each do |crn|
+			puts "  -> #{crn[:title]} (CRN: #{crn[:crn]})".color(:magenta)
+		end
+
+		#Prompt for CRN
+		alt = (crns.length > 0)  ? " (or just type 'start') " : " "
+		input = ask("\nEnter a CRN to add it#{alt}".color(:green) + ":: ") { |q| q.echo = true }
+
+		#Validate CRN to be 5 Digits 
+		if (input =~ /^\d{5}$/) then
+		
+			#Display CRN Info
+			c = getCourse(input.to_s)
+			puts "\nCourse: #{c[:title]} - #{c[:crn]}".color(:red)
+			puts "--> Time: #{c[:begin]}-#{c[:end]} on #{c[:days]}".color(:cyan)
+			puts "--> Teacher: #{c[:instructor]}".color(:cyan)
+			puts "--> Type: #{c[:type]} || Status: #{c[:status]}".color(:cyan)
+			puts "--> Availability: #{c[:seats]} / #{c[:capacity]}\n".color(:cyan)
+
+			#Add Class Prompt
+			add = ask("Add This Class? (yes/no)".color(:yellow) + ":: ") { |q| q.echo = true }
+			crns.push(c) if (add =~ /yes/)
+
+		elsif (input == "start") then
+			checkCourses(crns)
+		end 
+	end
+end
+
 
 def main
 	system("clear")
@@ -153,61 +189,11 @@ def main
 
 	system("clear")
 	if login(username, password) then
-		puts "Login Successful\n--------------\n".color(:yellow)
+		addCourses
 	else
 		puts "Invalid PID/Password"
 		exit
 	end
-	
-
-	crns = []
-
-
-	loop do 
-
-		system("clear")
-		puts "Your CRNs:".color(:red)
-		crns.each do |crn|
-			puts "  -> #{crn[:title]} (CRN: #{crn[:crn]})"
-		end
-
-		if (crns.length > 0) then
-			alt = " (or just type 'start') "
-		else
-			alt = " "
-		end
-
-		
-		input = ask("\nEnter a CRN to add it#{alt}".color(:green) + ":: ") { |q| q.echo = true }
-
-		if (input =~ /^\d{5}$/) then
-			
-
-			c = getCourse(input.to_s)
-
-
-			print "\n"
-			puts "Course: #{c[:title]} - #{c[:crn]}".color(:red)
-			puts "--> Time: #{c[:begin]}-#{c[:end]} on #{c[:days]}".color(:cyan)
-			puts "--> Teacher: #{c[:instructor]}".color(:cyan)
-			puts "--> Type: #{c[:type]} || Status: #{c[:status]}".color(:cyan)
-			puts "--> Availability: #{c[:seats]} / #{c[:capacity]}".color(:cyan)
-			print "\n"
-
-			add = ask("Add This Class? (yes/no)".color(:yellow) + ":: ") { |q| q.echo = true }
-			if (add =~ /yes/) then 
-				crns.push(c)
-			end
-
-		elsif (input == "start") then
-			courseAdd(crns)
-		else 
-			puts "Invalid CRN, This is a 5 Digit Course Request Number"
-			
-		end
-	end
-
-
 end
 
 main
